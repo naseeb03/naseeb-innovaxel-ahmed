9fb35a4b57ef9f9236eccf0adfe03c607efbf627
Naseeb Ahmed <naseeb.devpro@gmail.com>
2025-03-22 12:16:44 +0500

    Apply URL validation in routes

--- backend/src/routes/urlRoutes.js
+++ backend/src/routes/urlRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { createShortUrl, deleteUrl, getOriginalUrl, getUrlStats, updateUrl } from "../controllers/urlController.js";
+import { validateUrl } from "../middlewares/validators.js";
 // import { createShortUrl, getOriginalUrl, updateUrl, deleteUrl, getUrlStats } from "../controllers/urlController.js";
 
 const router = express.Router();
 
-router.post("/shorten", createShortUrl);
+router.post("/shorten", validateUrl, createShortUrl);
 
 router.get("/shorten/:shortCode", getOriginalUrl);
 
-router.put("/shorten/:shortCode", updateUrl);
+router.put("/shorten/:shortCode", validateUrl, updateUrl);
 
 router.delete("/shorten/:shortCode", deleteUrl);
 
