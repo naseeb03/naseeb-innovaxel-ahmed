72ffce26fa419449be41db0c20a7224ad942503c
Naseeb Ahmed <naseeb.devpro@gmail.com>
2025-03-22 09:32:07 +0500

    URL routes made

---
+++ backend/src/routes/urlRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { createShortUrl, getOriginalUrl, updateUrl, deleteUrl, getUrlStats } from "../controllers/urlController.js";
+
+const router = express.Router();
+
+router.post("/shorten", createShortUrl);
+
+router.get("/shorten/:shortCode", getOriginalUrl);
+
+router.put("/shorten/:shortCode", updateUrl);
+
+router.delete("/shorten/:shortCode", deleteUrl);
+
+router.get("/shorten/:shortCode/stats", getUrlStats);
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./controllers/db.js";
+import connectDB from "./config/db.js";
+// import urlRoutes from "./routes/urlRoutes.js"
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+// app.use("/api", urlRoutes);
 
 app.get("/", (req, res) => {
     res.send("URL Shortener API is Running");
