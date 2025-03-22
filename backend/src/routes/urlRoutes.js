b8eb6b24f49d771250d11ecdbdb985130a596d41
Naseeb Ahmed <naseeb.devpro@gmail.com>
2025-03-22 12:19:57 +0500

    Improve code for better readability

--- backend/src/routes/urlRoutes.js
+++ backend/src/routes/urlRoutes.js
@@ -1,18 +1,19 @@
 import express from "express";
-import { createShortUrl, deleteUrl, getOriginalUrl, getUrlStats, updateUrl } from "../controllers/urlController.js";
+import { 
+  createShortUrl, 
+  getOriginalUrl, 
+  updateUrl, 
+  deleteUrl, 
+  getUrlStats 
+} from "../controllers/urlController.js";
 import { validateUrl } from "../middlewares/validators.js";
-// import { createShortUrl, getOriginalUrl, updateUrl, deleteUrl, getUrlStats } from "../controllers/urlController.js";
 
 const router = express.Router();
 
 router.post("/shorten", validateUrl, createShortUrl);
-
 router.get("/shorten/:shortCode", getOriginalUrl);
-
 router.put("/shorten/:shortCode", validateUrl, updateUrl);
-
 router.delete("/shorten/:shortCode", deleteUrl);
-
 router.get("/shorten/:shortCode/stats", getUrlStats);
 
 export default router;
